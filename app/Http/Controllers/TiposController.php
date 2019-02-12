579ae7e5d0e022e4c2ab264043a7078c5b54a5da
Joao Lucas Amorim <joaolucasamorim8@gmail.com>
2019-02-12 10:14:23 -0300

    Cadastro e Visao de Genero por ID do usuario

--- app/Http/Controllers/TiposController.php
+++ app/Http/Controllers/TiposController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 use App\Tipos;
 use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests\TiposFormRequest;
+use Auth;
 use DB;
 
 class TiposController extends Controller
-{
-     public function __construct(){
+{   
+
+    public function __construct(){
     	//
     }
 
@@ -20,51 +22,53 @@ class TiposController extends Controller
     		$tipos = DB::table('tipos')
     		->where('nome' , 'LIKE' , '%'.$query.'%')
             ->where('condicao' ,'=' , '1')
-    		->orderBy('id_tipo', 'desc')
-    		->paginate(7);
-    		return view('cadastros.tipos.index', [
-    			"tipos" => $tipos, "searchText"=>$query
-    		]);	
-    	}
+            ->where('usuario_id', '=' , '' . Auth::user()->id)
+            ->orderBy('id_tipo', 'desc')
+            ->paginate(7);
+            return view('cadastros.tipos.index', [
+             "tipos" => $tipos, "searchText"=>$query
+         ]);	
+        }
     }
 
-     public function create(){	
-     	return view ("cadastros.tipos.create");
-    }
+    public function create(){	
+      return view ("cadastros.tipos.create");
+  }
 
-     public function store(TiposFormRequest $request){
-     	$tipo = new Tipos;
-     	$tipo->nome=$request->get('nome');	
-        $tipo->condicao=1;
-     	$tipo->save();
+  public function store(TiposFormRequest $request){
+      $tipo = new Tipos;
+      $tipo->nome=$request->get('nome');	
+      $tipo->condicao=1;
+      $tipo->usuario_id=Auth::user()->id;
+      $tipo->save();
 
-     	return Redirect::to('cadastro/tipos');	
-    }
+      return Redirect::to('cadastro/tipos');	
+  }
 
-     public function show($id){
-     	return view("cadastros.tipos.show",
-     		[
-     			"tipo"=>Tipos::findOrFail($id)
-     		]);
-    }
+  public function show($id){
+      return view("cadastros.tipos.show",
+         [
+            "tipo"=>Tipos::findOrFail($id)
+        ]);
+  }
 
-     public function edit($id){
-     return view("cadastros.tipos.edit",
-     		[
-     			"tipo"=>Tipos::findOrFail($id)
-     		]);	
-    }
+  public function edit($id){
+   return view("cadastros.tipos.edit",
+     [
+        "tipo"=>Tipos::findOrFail($id)
+    ]);	
+}
 
-      public function update(TiposFormRequest $request, $id){
-        $tipo=Tipos::findOrFail($id);
-        $tipo->nome=$request->get('nome');
-        $tipo->update();
-        return Redirect::to('cadastro/tipos');
-    }
-     public function destroy($id){	
-     	$tipo=Tipos::findOrFail($id);
-     	$tipo->condicao = '0';     	
-     	$tipo->update();
-     	return Redirect::to('cadastro/tipos');
-    }
+public function update(TiposFormRequest $request, $id){
+    $tipo=Tipos::findOrFail($id);
+    $tipo->nome=$request->get('nome');
+    $tipo->update();
+    return Redirect::to('cadastro/tipos');
+}
+public function destroy($id){	
+  $tipo=Tipos::findOrFail($id);
+  $tipo->condicao = '0';     	
+  $tipo->update();
+  return Redirect::to('cadastro/tipos');
+}
 }
